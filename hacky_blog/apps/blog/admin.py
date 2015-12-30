f9eb865d6efeb82a86601ccbb3300cda30d69d46
Josué Alonzo Chavarría <joshi.alonzo@gmail.com>
2015-12-30 17:30:52 -0600

    1. registration of Post model in admin.py 2. creation of superuser for localhost/admin/

--- hacky_blog/apps/blog/admin.py
+++ hacky_blog/apps/blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Post
 
 # Register your models here.
+
+# https://docs.djangoproject.com/en/1.8/ref/contrib/admin/
+# admin package
+# site module inside admin
+# register function inside site module
+admin.site.register(Post)
