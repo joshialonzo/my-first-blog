905f5e78efad7cf164c0e850c4d6e887c30e189f
Josué Alonzo <joshi.alonzo@gmail.com>
2015-12-31 18:41:24 -0600

    use queryset for dynamic data in templates

--- hacky_blog/apps/blog/views.py
+++ hacky_blog/apps/blog/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 # Learn   more    about   Django  views   by  reading the official    documentation:
 # https://docs.djangoproject.com/en/1.8/topics/http/views/
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    # https://docs.djangoproject.com/en/1.8/ref/models/querysets/
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', { 'posts' : posts })
+
