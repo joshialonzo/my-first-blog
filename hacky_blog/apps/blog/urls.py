f7f307050e3f829266de37ed4e2105cd50895ce0
Josué Alonzo <joshi.alonzo@gmail.com>
2015-12-31 22:52:39 -0600

    added view and template for detailed blog post as well as CSS for the site

--- hacky_blog/apps/blog/urls.py
+++ hacky_blog/apps/blog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 # https://docs.djangoproject.com/en/1.8/topics/http/urls/
 urlpatterns = [
-    url(r'^$', views.post_list, name="post_list")
+    url(r'^$', views.post_list, name="post_list"),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
 ]
