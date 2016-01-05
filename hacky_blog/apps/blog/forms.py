7e0cdf632581cc9c1feba544574ad26846e5b3a8
Josué Alonzo <joshi.alonzo@gmail.com>
2016-01-05 06:24:38 -0600

    1. let your readers write comments 2. moderating your comments

--- hacky_blog/apps/blog/forms.py
+++ hacky_blog/apps/blog/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Post
+from .models import Post, Comment
 
 # https://docs.djangoproject.com/en/1.8/topics/forms/
 
@@ -8,3 +8,9 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'text')
+
+class CommentForm(forms.ModelForm):
+
+    class Meta:
+        model = Comment
+        fields = ('author', 'text',)
