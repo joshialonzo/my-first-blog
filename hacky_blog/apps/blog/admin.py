196e78d62b0369358ede65ff748b56b0202b4677
Josué Alonzo <joshi.alonzo@gmail.com>
2016-01-05 02:44:15 -0600

    1. creating comment blog model 2. create tables for models in your database 3. register comment model in admin panel

--- hacky_blog/apps/blog/admin.py
+++ hacky_blog/apps/blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 # Register your models here.
 
@@ -8,3 +8,4 @@ from .models import Post
 # site module inside admin
 # register function inside site module
 admin.site.register(Post)
+admin.site.register(Comment)
